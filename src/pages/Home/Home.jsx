01976379107cda237aa0cdc09c67ca51004563d0
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-12 18:29:13 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,39 @@
+import React from 'react'
+import "./Home.css"
+import Navbar from "../../components/Navbar/Navbar"
+import hero_banner from "../../assets/hero_banner.jpg"
+import hero_title from "../../assets/hero_title.png"
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png"
+import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner_img'/>
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img'/>
+          <p>Discovering his ties to a adventoures life style and fighting marvelous in a periodically amazing way for the battle win</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards/>
+        </div>
+      </div>
+      <div className="moreCards">
+      <TitleCards title={"Blockbuster Movies"}/>
+      <TitleCards title={"Top Picks for You"}/>
+      <TitleCards  title={"Popular"}/>
+      <TitleCards  title={"Upcoming"} />
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
 
