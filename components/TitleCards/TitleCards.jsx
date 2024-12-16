14bc3c6b518631f4f8567827eeb673127aa8dcf9
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-16 20:47:04 +0530

    Update TitleCards.jsx

--- components/TitleCards/TitleCards.jsx
+++ components/TitleCards/TitleCards.jsx
@@ -25,10 +25,10 @@ const TitleCards = ({title,category}) => {
       <h2>{title?title:"Populor on Netflix"}</h2>
       <div className="card-list">
          {apiData.map((card,index)=>{
-      return <div className="card" key={index}>
+       return <Link to={`/player/${card.id}`} className="card" key={index}>
         <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
         <p>{card.original_title}</p>
-      </div>
+      </Link>
          })}
       </div>
     </div>
