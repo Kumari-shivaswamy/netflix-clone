2a7e8e6949ac367d21c6fcd7a916514f68fbbb4c
Kumari S <118825518+Kumari-shivaswamy@users.noreply.github.com>
2024-12-16 20:40:39 +0530

    displaying api data in UI

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -13,7 +13,13 @@ const Player = () => {
 })
   return (
     <div className='player'>
-      
+      <img src={backArrow} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+    <div className="playerInfo">
+      <p>{apiData.published_at.slice(0,10)}</p>
+      <p>{apiData.name}</p>
+      <p>{apiData.typeof}</p>
     </div>
   )
 }
